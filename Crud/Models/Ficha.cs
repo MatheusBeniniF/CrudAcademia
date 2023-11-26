88574741306aaa328eed1853da72731f8079984c
Matheus <matheus_benini@hotmail.com>
2023-11-25 23:02:22 -0300

    att repo

--- Crud/Models/Ficha.cs
+++ Crud/Models/Ficha.cs
@@ -5,6 +5,6 @@ public class Ficha
     public int? Id { get; set; }
     public string Titulo { get; set; } = "";
     public string User { get; set; } = "";
-    
+    public string? Anotacoes { get; set; } = "";
     public IList<ExercicioRecord> Exercicios { get; set; } = new List<ExercicioRecord>();
-}
+}   
